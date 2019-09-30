ed45aaf26de47cd5f5a804c03de63378e1c26efa
Douglas Patton <50275427+DouglasPatton@users.noreply.github.com>
2019-09-30 15:21:23 -0400

    save and no-save version

--- scrape_inputs.py
+++ scrape_inputs.py
@@ -5,9 +5,23 @@ import time
 timestr = time.strftime("%Y%m%d-%H%M%S")
 
 import os
+
+#save version:
+"""
 dir_path=os.path.dirname(os.path.realpath(__file__))
 base_filename=dir_path
 filename, headers = urllib.request.urlretrieve(url, filename=dir_path+"\\data\\input_param_{}.py".format(timestr))
 input_params=open(filename).read()
-print(input_params[0:100])
+b=[line for line in open(filename)]"""
+
+#skip saving
+line_list=[line for line in urllib.request.urlopen(url)]
+
+import re
+
+
+
+
+
+
 
