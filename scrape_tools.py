6ea8cb76464d06ed39c0d66e36123d2b5c9ab89b
Douglas Patton <50275427+DouglasPatton@users.noreply.github.com>
2019-10-03 08:32:17 -0400

    turned into an object, other small fixes

--- scrape_tools.py
+++ scrape_tools.py
@@ -7,26 +7,40 @@ timestr = time.strftime("%Y%m%d-%H%M%S")
 import os
 
 
-def pull_overview(model=None,submodel=None,baseurl=None,doc_type='input_parameters'):
-    """
-    """
-    if model==None:model='Meteorology' #default to meterology model
-    if submodel==None:submodel='Precipitation'
-    if baseurl==None: #default to dev branch, but user can provide own url, such as for master branch
-        #may be assuming wrong structure since other models have overview in "views.py"
-        baseurl="https://raw.githubusercontent.com/quanted/hms_app/dev/models/{}/{}_overview.py".format(model.lower(),submodel.lower())
-    dir_path=os.path.dirname(os.path.realpath(__file__))
-    base_filename=dir_path
-    filename, headers = urllib.request.urlretrieve(baseurl, filename=dir_path+"\\data\\{}_overview_{}.py".format(submodel.lower(),timestr))
-    
-    with open(filename,'rU') as pulled_pydoc:
-        #use .strip() method to remove line-endings and whitespace
-        all_lines=[line.strip() for line in pulled_pydoc]
+class Documentation_check():
+    def __init__(self,doc_type=None,model=None,submodel=None,url=None):
+        if doc_type==None:doc_type='input_parameters'
+        self.doc_type=doc_type
+        if model==None:model='Meteorology'
+        self.model=model
+        if submodel==None:submodel='Precipitation'
+        self.submodel=submodel
+        self.url=url#not very flexible with urls
+
+    #def doc_compare(    
+
+    def pull_overview(self,model=None,submodel=None,baseurl=None,doc_type='input_parameters'):
+        """creates self.pull_overview_dict 
+        """
+        if model==None:model='Meteorology' #default to meterology model
+        if submodel==None:submodel='Precipitation'
+        if baseurl==None: #default to dev branch, but user can provide own url, such as for master branch
+            #may be assuming wrong structure since other models have overview in "views.py"
+            baseurl="https://raw.githubusercontent.com/quanted/hms_app/dev/models/{}/{}_overview.py".format(model.lower(),submodel.lower())
+        dir_path=os.path.dirname(os.path.realpath(__file__))
+        base_filename=dir_path
+        try:
+            filename, headers = urllib.request.urlretrieve(baseurl, filename=dir_path+"\\data\\{}_overview_{}.py".format(submodel.lower(),timestr))
+        except:print('url error. url:',baseurl)
+        with open(filename,'rU') as pulled_pydoc:
+            #use .strip() method to remove line-endings and whitespace
+            all_lines=[line.strip() for line in pulled_pydoc]
         #remove all lines that have been commented out
         all_lines=[line for line in all_lines if not re.search('^\#',line)]#assuming \\
         #just one occurrence of line in all_lines, make flexible later
+        #doc_pull_start=[i for i,line in enumerate(all_lines) if re.search('^'+doc_type+'\s\=\s\[',line)];print(doc_pull_start)
         doc_pull_start=[i for i,line in enumerate(all_lines) if re.search('^'+doc_type+'\s\=\s\[',line)][0]
-        doc_pull_end,end_char_position_in_line=parenth_counter(all_lines[doc_pull_start+1:],'[')#changing \\
+        doc_pull_end,end_char_position_in_line=self.parenth_counter(all_lines[doc_pull_start+1:],'[')#changing \\
         #default open parenth to square bracket, make flexible later
         doc_pull_end+=1+doc_pull_start#add 1 because we did not give parentheses_counter \\
         #the rest of the line that the open parenth started on
@@ -67,37 +81,44 @@ def pull_overview(model=None,submodel=None,baseurl=None,doc_type='input_paramete
                 keylist=['name','type','description','child_elements']
                 for i in range(len(end_quote_loc)):
                     pull_overview_dict[keylist[i]]=line[start_quote_loc[i]+1:end_quote_loc[i]]
-                print(pull_overview_dict)
-                
-        
+        self.pull_overview_dict=pull_overview_dict
+                    
+            
 
 
 
-def pull_input_params(model=None,baseurl=None):
-    """downloads model_parameters.py file
-    downloads from master repo as default
-    """
-    if model==None:model='Meteorology' #default to meterology model
-    if baseurl==None: #default to dev branch, but user can provide own url, such as for master branch
-        baseurl="https://raw.githubusercontent.com/quanted/hms_app/dev/models/{}/{}_parameters.py".format(model.lower(),model.lower())
-    dir_path=os.path.dirname(os.path.realpath(__file__))
-    base_filename=dir_path
-    filename, headers = urllib.request.urlretrieve(baseurl, filename=dir_path+"\\data\\{}_input_param_{}.py".format(model.lower(),timestr))
+    def pull_input_params(self,model=None,baseurl=None):
+        """downloads model_parameters.py file
+        downloads from master repo as default
+        creates self.submodel_dict, a dictionary
+        containing dictionaries of parameters
+        for each submodel in the specified model
+        """
+        if model==None:model='Meteorology' #default to meterology model
+        if baseurl==None: #default to dev branch, but user can provide own url, such as for master branch
+            baseurl="https://raw.githubusercontent.com/quanted/hms_app/dev/models/{}/{}_parameters.py".format(model.lower(),model.lower())
+        dir_path=os.path.dirname(os.path.realpath(__file__))
+        base_filename=dir_path
+        filename, headers = urllib.request.urlretrieve(baseurl, filename=dir_path+"\\data\\{}_input_param_{}.py".format(model.lower(),timestr))
 
-    with open(filename,'rU') as input_params:
-        #use .strip() method to remove line-endings and whitespace
-        all_lines=[line.strip() for line in input_params]
-        all_lines=[line for line in all_lines if not re.search('^\#',line)]#remove all lines that have been commented out
+        with open(filename,'rU') as input_params:
+            #use .strip() method to remove line-endings and whitespace
+            all_lines=[line.strip() for line in input_params]
+        #remove all lines that have been commented out
+        all_lines=[line for line in all_lines if not re.search('^\#',line)]
+
+        #put widgets on single line
+        #feature_dict_line_pos,feature_dict_lines=self.my[(ii,line) for ii,line in enumerate(feature_lines)]
                
         #extract submodel name and position in all_lines
         submodel_class_pos=[]
-        submodel_class_pos,submodel_class_line=my_unzip([(i,line) for i,line in enumerate(all_lines) if re.search('^class',line)])
+        submodel_class_pos,submodel_class_line=self.my_unzip([(i,line) for i,line in enumerate(all_lines) if re.search('^class',line)])
         assert not submodel_class_pos==[],'there are no classes defined in this file'
         submodel_name_end=[re.search('FormInput',line).start() for line in submodel_class_line]
         submodel_name_list=[line[6:end] for line,end in zip(submodel_class_line,submodel_name_end)]
         #print('submodel_class_pos',submodel_class_pos)
               
-        submodel_dict={}
+        submodel_dict={}#this will be a collection of dictionaries for each submodel
         
         for i,submodel_name in enumerate(submodel_name_list):
             if i<len(submodel_name_list)-1:
@@ -107,45 +128,33 @@ def pull_input_params(model=None,baseurl=None):
 
             
             #for the range of lines for each submodel, find the parameters
-            param_line_pos, param_lines=my_unzip([(i,line) for i,line in enumerate(submodel_lines) if re.search('\s\=\sforms\.',line)])
+            param_line_pos, param_lines=self.my_unzip([(i,line) for i,line in enumerate(submodel_lines) if re.search('\s\=\sforms\.',line)])
             param_name_end=[re.search('\s\=\sforms\.',line).start() for line in param_lines]
 
 
             features_start=[i+1 for i in param_line_pos]
             features_end=[]#initialize and then find the end of each feature based on line containing closing parenth
             for j in features_start:
-                line_count_after_open,pos_in_line=parenth_counter(submodel_lines[j+1:]) #returns the line
+                line_count_after_open,pos_in_line=self.parenth_counter(submodel_lines[j+1:]) #returns the line
                 #(and position in the line which I'm not using) of the closing partenth, assuming it doesn't
                 #close on the same line. modify this line to make flexible,
                 #parenth_counter already flexible
                 line_count_after_open+=1#add 1 since line with opening parenth not given to parenth_counter
                 features_end.append(j+line_count_after_open)
-                
-
-            '''this code removed because was not flexible to having close paranetheses not on it's own line
-            for j in features_start:
-                ij=0
-                while True:
-                    if re.search('^\)$',submodel_lines[j+ij]):
-                        #end_found==True
-                        features_end.append(j+ij)
-                        break
-                    else:ij+=1
-                #features_end.append=[i for i,line in enumerate(feature_end_search_lines) if re.search('^\)$',line)]
-''' 
+         
             
             #some submodels may not have parameters, so start if statement for submodels with non-empty lists of parameters.
             if param_name_end==[]:param_dict='no_parameters'
             else: 
                 param_name_list=[line[:end] for line,end in zip(param_lines,param_name_end)]
-                print(submodel_name,'param_name_list:',param_name_list)
+                #print(submodel_name,'param_name_list:',param_name_list)
                 param_dict={}#initialize and then build param_dict
                 for ii,param_name in enumerate(param_name_list):
                     #define range for featuers of each parameter
                     paramfeaturelines=submodel_lines[features_start[ii]:features_end[ii]]#list of lines for each paramter's potential features
-                    feature_line_pos,feature_lines=my_unzip([(ii,line) for ii,line in enumerate(paramfeaturelines) if re.search('\=',line)])
+                    feature_line_pos,feature_lines=self.my_unzip([(ii,line) for ii,line in enumerate(paramfeaturelines) if re.search('\=',line)])
                     feature_name_end=[re.search('\=',line).start() for line in feature_lines]
-                    feature_name, feature_value=my_unzip([(line[:end],line[end:]) for line,end in zip(feature_lines,feature_name_end)])
+                    feature_name, feature_value=self.my_unzip([(line[:end],line[end:]) for line,end in zip(feature_lines,feature_name_end)])
                     param_feature_dict={}
                     for key,value in zip(feature_name,feature_value):
                         param_feature_dict[key]=value
@@ -155,45 +164,51 @@ def pull_input_params(model=None,baseurl=None):
             submodel_dict[submodel_name]=param_dict
         #print(submodel_dict)
 
-    print(submodel_dict)
+        #print(submodel_dict)
+        self.submodel_dict=submodel_dict
 
 
 
 
+    def my_unzip(self,list_of_tups):
+        #returns a tupple of 2 empty lists rather than just an empty list if I used zip(*...) on ([])
+        unzipped=zip(*list_of_tups)
+        if len([i for i in unzipped])==0:#easier way to unpack and check if zip object is empty?
+            return [],[]
+        else:return zip(*list_of_tups)
 
-def my_unzip(list_of_tups):
-    #returns a tupple of 2 empty lists rather than just an empty list if I used zip(*...) on ([])
-    unzipped=zip(*list_of_tups)
-    if len([i for i in unzipped])==0:#easier way to unpack and check if zip object is empty?
-        return [],[]
-    else:return zip(*list_of_tups)
-
-def parenth_counter(text_after_open_p,open_type=None):
-    '''takes a list of strings and 
-    '''
-    #print('open_type:',open_type)
-    if open_type==None:open_type='('
-    if open_type=='(':close_type=')'
-    if open_type=='[':close_type=']'
-    if open_type=='{':close_type='}'
-    open_count_from_zero=1
-    line_count=0
-    while open_count_from_zero>0:
-        line=text_after_open_p[line_count]
-        char_count=0
-        for i in line:
-            if i==close_type:
-                open_count_from_zero-=1  
-            if i==open_type:
-                open_count_from_zero+=1
-            if open_count_from_zero==0:break;break#twice since nested loop
-            char_count+=1
-        line_count+=1    
-    return line_count,char_count
+    def parenth_counter(self,text_after_open_p,open_type=None):
+        '''takes a list of strings and returns a tuple with the
+        line and character position in text_after of the closing parenth
+        '''
+        #print('open_type:',open_type)
+        if open_type==None:open_type='('
+        if open_type=='(':close_type=')'
+        if open_type=='[':close_type=']'
+        if open_type=='{':close_type='}'
+        open_count_from_zero=1
+        line_count=0
+        while open_count_from_zero>0:
+            line=text_after_open_p[line_count]
+            char_count=0
+            for i in line:
+                if i==close_type:
+                    open_count_from_zero-=1  
+                if i==open_type:
+                    open_count_from_zero+=1
+                if open_count_from_zero==0:break;break#twice since nested loop
+                char_count+=1
+            line_count+=1    
+        return line_count,char_count
 
 if __name__=="__main__":
-    pull_overview()
-    
+    test=Documentation_check()
+    the_model='Meteorology';the_submodel='Precipitation';the_doc_type='input_parameters'
+    test.pull_overview(model=the_model,submodel=the_submodel,doc_type=the_doc_type)
+    test.pull_input_params(model=the_model)
+    print('submodel_dict:',test.submodel_dict)
+    print('##########################')
+    print('pull_overview_dict:',test.pull_overview_dict)
 
 
 
--- scraper_main.py
+++ scraper_main.py
@@ -4,5 +4,7 @@ import scrape_tools as st
 #input_parameters=st.pull_input_params('meteorology')
 
 print('------------------------')
+test=st.Documentation_check()
+test.pull_overview(model='Meteorology',submodel='Precipitation',doc_type='input_parameters')
 
-st.pull_overview()
+test.pull_input_params(model='Meteorology')
