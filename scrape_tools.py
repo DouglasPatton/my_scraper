fa26026fdb79a17a952811b77861f4ade61472cb
Douglas Patton <50275427+DouglasPatton@users.noreply.github.com>
2019-10-03 15:53:33 -0400

    fixed a few bugs and lots of condensing

--- scrape_tools.py
+++ scrape_tools.py
@@ -1,13 +1,12 @@
 import urllib.request
-url="https://raw.githubusercontent.com/quanted/hms_app/master/models/meteorology/meteorology_parameters.py"
 import re
-import time
-timestr = time.strftime("%Y%m%d-%H%M%S")
-
+from time import strftime
+timestr = strftime("%Y%m%d-%H%M%S")
+url="https://raw.githubusercontent.com/quanted/hms_app/dev/models/"
 import os
 
 
-class Documentation_check():
+class Documentation_Check():
     """gathers parameters, gathers existing documentation, compares
     methods
         pull_overview
@@ -16,22 +15,46 @@ class Documentation_check():
             creates submodel_dict
         do_doc_compare
     """
-    def __init__(self,doc_type=None,model=None,submodel=None,url=None):
+    def __init__(self,doc_type=None,model=None,submodel=None,branch=None):
         if doc_type==None:doc_type='input_parameters'
         self.doc_type=doc_type
         if model==None:model='Meteorology'
         self.model=model
         if submodel==None:submodel='Precipitation'
         self.submodel=submodel
-        self.url=url#not very flexible with urls
-    def doc_compare_to_table(self)
-        import pandas as pd
-        try:
-            self.compare_submodel_input_param_doc_dict
-        except:
-            print('self.compare_submodel_input_param_doc_dict does not exist, running with defaults')
-            self.do_doc_compare()
+        if branch==None:branch='dev'
+        self.url="https://raw.githubusercontent.com/quanted/hms_app/{}/models/".format(branch)
         
+    def doc_compare_to_table(self,submodel_comparison_dict=None):
+        #import pandas as pd
+        if submodel_comparison_dict==None:
+            try:
+                self.compare_submodel_input_param_doc_dict
+            except:
+                print('self.compare_submodel_input_param_doc_dict does not exist, running with defaults')
+                self.do_doc_compare()
+            submodel_comparison_dict=self.compare_submodel_input_param_doc_dict
+        #extract title:value from inputs that have that widget
+        #then extract type and description from docs
+        simplified_submodel_dict={}
+        
+        for param,param_dict in submodel_comparison_dict.items():
+            simplified_param_dict={}
+            try:
+                simplified_param_dict['widget_title']=param_dict['from_input']['widget_title']
+            except:
+                simplified_param_dict['widget_title']='N/A'
+            try:
+                simplified_param_dict['type']=param_dict['from_doc']['type']
+            except:
+                simplified_param_dict['type']="_________________________________"
+            try:
+                simplified_param_dict['description']=param_dict['from_doc']['description']
+            except:
+                simplified_param_dict['description']="_________________________________"
+            simplified_submodel_dict[param]=simplified_param_dict
+        self.compare_doc_simple_dict=simplified_submodel_dict    
+        #self.compare_doc_simple_table=pd.DataFrame(simplified_dict).T
     
     def do_doc_compare(self, model=None,submodel=None):
         '''
@@ -82,7 +105,12 @@ class Documentation_check():
             if input_param_has_doc==0:
                  orphaned_input_parameters.append(input_parameter)#the dictionary will also be missing \\
                  #key:value for the doc side but not the input side
-        self.compare_orphaned_doc_params=[doc_parameter for doc_parameter,doc_feature_dict in self.pull_overview_dict.items() if not doc_parameter in docs_with_inputs ]
+        self.compare_orphaned_doc_params=[
+            doc_parameter 
+            for doc_parameter,doc_feature_dict 
+            in self.pull_overview_dict.items() 
+            if not doc_parameter in docs_with_inputs
+            ]
         self.compare_orphaned_input_parameters=orphaned_input_parameters
         self.compare_submodel_input_param_doc_dict=submodel_input_param_doc_dict
         
@@ -99,27 +127,31 @@ class Documentation_check():
         if submodel==None:submodel='Precipitation'
         if baseurl==None: #default to dev branch, but user can provide own url, such as for master branch
             #may be assuming wrong structure since other models have overview in "views.py"
-            baseurl="https://raw.githubusercontent.com/quanted/hms_app/dev/models/{}/{}_overview.py".format(model.lower(),submodel.lower())
+            baseurl=self.url+"{}/{}_overview.py".format(model.lower(),submodel.lower())
         dir_path=os.path.dirname(os.path.realpath(__file__))
         base_filename=dir_path
         try:
-            filename, headers = urllib.request.urlretrieve(baseurl, filename=dir_path+"\\data\\{}_overview_{}.py".format(submodel.lower(),timestr))
-        except:print('url error. url:',baseurl)
+            filename, headers = urllib.request.urlretrieve(
+                baseurl,
+                filename=dir_path+"\\data\\{}_overview_{}.py".format(submodel.lower(),timestr)
+                )
+        except:
+            print('url error. url:',baseurl)
         with open(filename,'rU') as pulled_pydoc:
             #use .strip() method to remove line-endings and whitespace
             all_lines=[line.strip() for line in pulled_pydoc]
         #remove all lines that have been commented out
         all_lines=[line for line in all_lines if not re.search('^\#',line)]#assuming \\
         #just one occurrence of line in all_lines, make flexible later
-        #doc_pull_start=[i for i,line in enumerate(all_lines) if re.search('^'+doc_type+'\s\=\s\[',line)];print(doc_pull_start)
         doc_pull_start=[i for i,line in enumerate(all_lines) if re.search('^'+doc_type+'\s\=\s\[',line)][0]
-        doc_pull_end,end_char_position_in_line=self.parenth_counter(all_lines[doc_pull_start+1:],'[')#changing \\
-        #default open parenth to square bracket, make flexible later
+        doc_pull_end,end_char_position_in_line=self.parenth_counter(all_lines[doc_pull_start+1:],'[')
         doc_pull_end+=1+doc_pull_start#add 1 because we did not give parentheses_counter \\
         #the rest of the line that the open parenth started on
         doc_pull_lines=all_lines[doc_pull_start+1:doc_pull_end-1]
-        #print(doc_pull_start,doc_pull_end)
-        #print(len(doc_pull_lines))
+
+
+        #trying to condense string that spill over to next line into single line.
+        #doesn' seem to work
         doc_pull_lines_condensed=[]
         for i,line in enumerate(doc_pull_lines):
             if re.search('^\[',line):
@@ -130,9 +162,7 @@ class Documentation_check():
                 if line[-1]=="\"":
                     line=line[1:-1];print('here!')
                 doc_pull_lines_condensed[-1]=doc_pull_lines_condensed[-1]+line
-        #print(len(doc_pull_lines_condensed))
-        '''feature_doc_pos,feature_doc_lines=myunzip([(i,line) for i,line in enumerate(doc_pull_lines)if re.search('^\[',line)])
-        if len(doc_pull_lines)>len(feature_doc_lines):'''
+        
         if doc_type=='input_parameters':
             pull_overview_dict={}
             for line in doc_pull_lines_condensed:
@@ -142,21 +172,16 @@ class Documentation_check():
 
                 start_quote_loc=[0]
                 end_quote_loc=[]#add ending location at the end of loop
-                #line=line[1:-1]#drop first and last since already accounted for in start and end quote loc vars
                 for i,char in enumerate(line[:-1]):
                     if char=="\"":
                         if line[i+1]==",":#if next item is a comma
                             end_quote_loc.append(i)
                         if (i>2 and (line[i-2]=="," or line[i-1]==",")):
                             start_quote_loc.append(i)
-                end_quote_loc.append(len(line))
-                #print('start',start_quote_loc)
-                #print('end',end_quote_loc)
+                end_quote_loc.append(len(line))#as mentioned above
                 
                 keylist=['name','type','description','child_elements']
                 for i in range(len(end_quote_loc)-1):#-1 b/c first item is name
-                    #print(i)
-                    #print(keylist[i])
                     param_dict[keylist[i+1]]=line[start_quote_loc[i+1]+1:end_quote_loc[i+1]]
                 pull_overview_dict[line[start_quote_loc[0]+1:end_quote_loc[0]]]=param_dict
         self.pull_overview_dict=pull_overview_dict
@@ -165,29 +190,30 @@ class Documentation_check():
 
     def condense_widgets(self,all_lines):
         
-        widget_lines_start_pos,widget_lines=self.my_unzip([(ii,line) for ii,line in enumerate(all_lines) if re.search('widget\=.+\{$',line)])
-        widget_lines_end_shift, char=self.my_unzip([self.parenth_counter(all_lines[i+1:],'{') for i in widget_lines_start_pos])
-        #widget_lines_end_shift=[i+1 for i in widget_lines_end_shift]#add 1 to each b/c parenth_counter didn't have line 0, and convert to list
-
+        widget_lines_start_pos,widget_lines=self.my_unzip([
+            (ii,line) for ii,line
+            in enumerate(all_lines)
+            if re.search('widget\=.+\{$',line)
+            ])
+        widget_lines_end_shift, char=self.my_unzip([
+            self.parenth_counter(all_lines[i+1:],'{')
+            for i in widget_lines_start_pos
+            ])
+        
         widget_lines_condensed=[]
         for i,line in enumerate(widget_lines):
             for ii in range(widget_lines_end_shift[i]):
                 line=line+all_lines[widget_lines_start_pos[i]+1+ii]
-            widget_lines_condensed.append(line)
+            widget_lines_condensed.append(line)        
         
-        #print('full widget line',widget_lines_condensed)
         for i,pos in enumerate(widget_lines_start_pos):
             all_lines[pos]=widget_lines_condensed[i]
             for ii in range(widget_lines_end_shift[i]):
-                #print('line to be deleted:',all_lines[pos+ii+1])
                 all_lines[pos+ii+1]=""
-            #print(widget_lines_condensed[i])
             if re.search('\'title\'\:',widget_lines_condensed[i]):
-                all_lines[pos+1]='widget_title='+all_lines[pos][re.search('\'title\'\:',widget_lines_condensed[i]).start()+10:-3]
-                #print('here',all_lines[pos+1])
-                    
-                    
-        #print([all_lines[widget_lines_start_pos[i]] for i in range(len(widget_lines_start_pos))])
+                all_lines[pos+1]='widget_title='+all_lines[pos][
+                    re.search('\'title\'\:',widget_lines_condensed[i]).start()+10:-3
+                    ]
         return all_lines
     
     def pull_input_params(self,model=None,baseurl=None):
@@ -199,7 +225,7 @@ class Documentation_check():
         """
         if model==None:model='Meteorology' #default to meterology model
         if baseurl==None: #default to dev branch, but user can provide own url, such as for master branch
-            baseurl="https://raw.githubusercontent.com/quanted/hms_app/dev/models/{}/{}_parameters.py".format(model.lower(),model.lower())
+            baseurl=self.url+"/{}/{}_parameters.py".format(model.lower(),model.lower())
         dir_path=os.path.dirname(os.path.realpath(__file__))
         base_filename=dir_path
         filename, headers = urllib.request.urlretrieve(baseurl, filename=dir_path+"\\data\\{}_input_param_{}.py".format(model.lower(),timestr))
@@ -212,18 +238,23 @@ class Documentation_check():
 
         #put widgets that span multiple lines on single line
         all_lines=self.condense_widgets(all_lines)
-        
-             
+                     
         #extract submodel name and position in all_lines
         submodel_class_pos=[]
-        submodel_class_pos,submodel_class_line=self.my_unzip([(i,line) for i,line in enumerate(all_lines) if re.search('^class',line)])
+        submodel_class_pos,submodel_class_line=self.my_unzip([
+            (i,line) for i,line
+            in enumerate(all_lines)
+            if re.search('^class',line)
+            ])
         assert not submodel_class_pos==[],'there are no classes defined in this file'
         submodel_name_end=[re.search('FormInput',line).start() for line in submodel_class_line]
         submodel_name_list=[line[6:end] for line,end in zip(submodel_class_line,submodel_name_end)]
-        #print('submodel_class_pos',submodel_class_pos)
-              
+
+        #check which submodels inherit prameters from hydrology submodel
+        self.input_ischildofhydrology=[submodel_name_list[i] for i,line in enumerate(submodel_class_line) if re.search('^class.+\(HydrologyFormInput\)\:$',line)]
+
+                      
         submodel_dict={}#this will be a collection of dictionaries for each submodel
-        
         for i,submodel_name in enumerate(submodel_name_list):
             if i<len(submodel_name_list)-1:
                 submodel_lines=all_lines[submodel_class_pos[i]+1:submodel_class_pos[i+1]] #this one is from i to i+1(next clump of lines)
@@ -241,34 +272,44 @@ class Documentation_check():
             for j in features_start:
                 line_count_after_open,pos_in_line=self.parenth_counter(submodel_lines[j+1:]) #returns the line
                 #(and position in the line which I'm not using) of the closing partenth, assuming it doesn't
-                #close on the same line. modify this line to make flexible,
-                #parenth_counter already flexible
+                #close on the same line. 
                 line_count_after_open+=1#add 1 since line with opening parenth not given to parenth_counter
                 features_end.append(j+line_count_after_open)
          
             
             #some submodels may not have parameters, so start if statement for submodels with non-empty lists of parameters.
-            if param_name_end==[]:param_dict='no_parameters'
+            if param_name_end==[]:
+                param_dict='no_parameters'
             else: 
                 param_name_list=[line[:end] for line,end in zip(param_lines,param_name_end)]
-                #print(submodel_name,'param_name_list:',param_name_list)
+                
                 param_dict={}#initialize and then build param_dict
                 for ii,param_name in enumerate(param_name_list):
                     #define range for featuers of each parameter
                     paramfeaturelines=submodel_lines[features_start[ii]:features_end[ii]]#list of lines for each paramter's potential features
-                    feature_line_pos,feature_lines=self.my_unzip([(ii,line) for ii,line in enumerate(paramfeaturelines) if re.search('\=',line)])
+                    feature_line_pos,feature_lines=self.my_unzip([
+                        (ii,line) for ii,line
+                        in enumerate(paramfeaturelines)
+                        if re.search('\=',line)
+                        ])
                     feature_name_end=[re.search('\=',line).start() for line in feature_lines]
-                    feature_name, feature_value=self.my_unzip([(line[:end],line[end+1:]) for line,end in zip(feature_lines,feature_name_end)])
+                    feature_name, feature_value=self.my_unzip([
+                        (line[:end],line[end+1:]) for line,end
+                        in zip(feature_lines,feature_name_end)
+                        ])
                     param_feature_dict={}
                     for key,value in zip(feature_name,feature_value):
                         param_feature_dict[key]=value
                     param_dict[param_name]=param_feature_dict
-                    #end for ii
-            
+                    
             submodel_dict[submodel_name]=param_dict
-        #print(submodel_dict)
 
-        #print(submodel_dict)
+        #add parameters and feature_dict from hydrology inputs to child inputs
+            #not flexible to other inheritance patterns
+        for submodel_name,param_dict in submodel_dict.items():
+            if submodel_name in self.input_ischildofhydrology:
+                for param,param_feature_dict in submodel_dict['Hydrology'].items():
+                    submodel_dict[submodel_name][param]=param_feature_dict
         self.submodel_dict=submodel_dict
 
 
@@ -306,12 +347,16 @@ class Documentation_check():
         return line_count,char_count
 
 if __name__=="__main__":
-    test=Documentation_check()
+    test=Documentation_Check()
     the_model='Meteorology';the_submodel='Precipitation';the_doc_type='input_parameters'
     test.pull_overview(model=the_model,submodel=the_submodel,doc_type=the_doc_type)
     test.pull_input_params(model=the_model)
     test.do_doc_compare(model=the_model,submodel=the_submodel)
+    test.doc_compare_to_table()
     print(test.compare_submodel_input_param_doc_dict)
+    print('------------------------')
+    print(test.compare_doc_simple_dict)
+    #print(test.compare_doc_simple_table)#no pandas on my computer to check
     
 ''' print('============model has finished=================')
     print('=============================')
--- scraper_main.py
+++ scraper_main.py
@@ -4,10 +4,13 @@ import scrape_tools as st
 #input_parameters=st.pull_input_params('meteorology')
 
 print('------------------------')
-test=Documentation_check()
+test=st.Documentation_Check()
 the_model='Meteorology';the_submodel='Precipitation';the_doc_type='input_parameters'
 test.pull_overview(model=the_model,submodel=the_submodel,doc_type=the_doc_type)
 test.pull_input_params(model=the_model)
 test.do_doc_compare(model=the_model,submodel=the_submodel)
+test.doc_compare_to_table()
 print(test.compare_submodel_input_param_doc_dict)
-   
+print('------------------------')
+print(test.compare_doc_simple_dict)
+#print(test.compare_doc_simple_table)
